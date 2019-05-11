a22a33c26c95715685ef70439a69779891a0c664
dasbus <90valentin@gmail.com>
2019-05-11 16:49:18 -0700

    moved cache outside HousingContainer

--- client/Containers/HousingContainer.jsx
+++ client/Containers/HousingContainer.jsx
@@ -1,49 +1,38 @@
 import React, { Component } from 'react';
 import Bracket from '../Components/Bracket.jsx';
+import lib from '../../lib/functions'
 
+
+let cache = {}
 
 class HousingContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
-		this.cache = {};
-	}
-
-	randomize(arr) {
-		let newArr = [];
-		arr.forEach((value, index) => {
-			newArr.push({ player: value, aNumber: (Math.random() * 100) })
-		})
-		newArr.sort(function (a, b) { return b.aNumber - a.aNumber })
-		let lastArr = [];
-		newArr.forEach((value) => { lastArr.push(value.player) })
-		return lastArr
 	}
 
 	render() {
-		console.log('props', this.props.functionality)
-		this.playersArr = this.randomize(this.props.string.split(", ")).slice();
+		let	playersArr = lib.randomize(this.props.string.split(", ")).slice();
 
 		let functionalityVariable = this.props.functionality;
 		let round = this.props.round
-		this.players = this.playersArr.map((each, index) => {
+		let players = playersArr.map((each, index) => {
 			let randomPokeGenerator = Math.floor(Math.random() * 150)
-			// let comp = React.createElement(Bracket);
-			if (!this.cache[each]) {
-				this.cache[each] = randomPokeGenerator;
+			if (!cache[each]) {
+				cache[each] = randomPokeGenerator;
 			}
 			return React.createElement(Bracket, {
 				onePlayerName: each,
 				key: index + 1,
 				className: 'cl' + (index + 1),
 				functionality: functionalityVariable,
-				pokeId: this.cache[each]
+				pokeId: cache[each]
 			})
 		})
 
 		return (
 			<div className="play-area">
-				{this.players.slice()}
+				{players.slice()}
 			</div>
 		);
 	}
