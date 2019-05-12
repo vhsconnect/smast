1d050ac5d603b457b547c6c9a97bb198fbd189d6
dasbus <90valentin@gmail.com>
2019-05-11 17:08:40 -0700

    changed HousingContainer to presentational

--- client/Containers/HousingContainer.jsx
+++ client/Containers/HousingContainer.jsx
@@ -1,41 +1,30 @@
 import React, { Component } from 'react';
 import Bracket from '../Components/Bracket.jsx';
 import lib from '../../lib/functions'
-
-
 let cache = {}
 
-class HousingContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+let HousingContainer = (props) => {
 
-	render() {
-		let	playersArr = lib.randomize(this.props.string.split(", ")).slice();
+	let playersArr = lib.randomize(props.string.split(", ")).slice();
+	let functionalityVariable = props.functionality;
+	let players = playersArr.map((each, index) => {
+		//if pokemonID hasnt been assigned to player, assign it then cache it
+		if (!cache[each]) cache[each] = Math.floor(Math.random() * 150)
 
-		let functionalityVariable = this.props.functionality;
-		let round = this.props.round
-		let players = playersArr.map((each, index) => {
-			let randomPokeGenerator = Math.floor(Math.random() * 150)
-			if (!cache[each]) {
-				cache[each] = randomPokeGenerator;
-			}
-			return React.createElement(Bracket, {
-				onePlayerName: each,
-				key: index + 1,
-				className: 'cl' + (index + 1),
-				functionality: functionalityVariable,
-				pokeId: cache[each]
-			})
+		return React.createElement(Bracket, {
+			onePlayerName: each,
+			key: index + 1,
+			className: 'cl' + (index + 1),
+			functionality: functionalityVariable,
+			pokeId: cache[each]
 		})
+	})
 
-		return (
-			<div className="play-area">
-				{players.slice()}
-			</div>
-		);
-	}
+	return (
+		<div className="play-area">
+			{players.slice()}
+		</div>
+	);
 }
 
 export default HousingContainer;
